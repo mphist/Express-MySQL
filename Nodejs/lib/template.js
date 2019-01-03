562d650a56eea1ce5f04d5ab4b8d835f8ca83414
Hooni <hooni@Hoonis-MacBook-Pro.local>
2019-01-03 17:30:09 -0500

    started web integration with "author" table

--- Nodejs/lib/template.js
+++ Nodejs/lib/template.js
@@ -15,7 +15,8 @@ module.exports = {
     </body>
     </html>
     `;
-  },list:function(filelist){
+  },
+  list:function(filelist){
     var list = '<ul>';
     var i = 0;
     while(i < filelist.length){
@@ -24,5 +25,23 @@ module.exports = {
     }
     list = list+'</ul>';
     return list;
+  },
+  authorSelect:function(authors, author_id=1) {
+    var tag = '';
+    var i = 0;
+    console.log(author_id);
+    while (i < authors.length) {
+      var author = authors[i];
+      var selected = '';
+      if (author_id === author.id) {
+        selected = ' selected';
+      }
+      tag += `<option value="${author.id}"${selected}>${author.name}</option>`;
+      i++;
+    }
+    console.log(tag);
+    return (
+    `<select name="authors">${tag}</select>`
+    );
   }
 }
--- Nodejs/routes/topic.js
+++ Nodejs/routes/topic.js
@@ -16,31 +16,46 @@ var connection = mysql.createConnection({
 connection.connect();
 
 router.get('/create', (request, response) => {
-  connection.query(`SELECT * FROM topic`, (error, rows, fields) => {
-    request.list = rows;
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.writeHead(200);
-    response.end(html);
-    });
+  connection.query(`SELECT * FROM topic`, (error, topics, fields) => {
+    if (error) {
+      throw error;
+    } else {
+      connection.query(`SELECT * FROM author`, (error2, authors) => {
+        if (error2) {
+          throw error2;
+        } else {
+          request.list = topics;
+          var title = 'WEB - create';       
+          var list = template.list(request.list);
+          var html = template.HTML(title, list, `
+            <form action="/topic/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                ${template.authorSelect(authors)}
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `, '');
+          response.writeHead(200);
+          response.end(html);
+        }
+      });
+    }
+  });
 });
 
 router.post('/create_process', (request, response) => {
   var post = request.body;
   var title = post.title;
   var description = post.description;
-  connection.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?,?,NOW(),?)`, [title, description, 1], (error, rows, fields) => {
+  var author = post.authors;
+  var query = connection.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?,?,NOW(),?)`, 
+    [title, description, author], (error, rows, fields) => {
     response.redirect(`/topic/${title}`);
   });
 });
@@ -56,26 +71,35 @@ router.get('/update/:pageId', (request, response) => {
         if (error2) {
           throw error2;
         } else {
-          request.result = result;
-          var title = filteredId;
-          var description = request.result[0].description;
-          var list = template.list(request.list);
-          var html = template.HTML(title, list,
-            `
-            <form action="/topic/update_process" method="post">
-              <input type="hidden" name="id" value="${request.result[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-          );
-          response.send(html);
+          connection. query(`SELECT * FROM author`, (error3, authors) => {
+            if (error3) {
+              throw error3;
+            } else {
+                request.result = result;
+                var title = filteredId;
+                var description = request.result[0].description;
+                var list = template.list(request.list);
+                var html = template.HTML(title, list,
+                  `
+                  <form action="/topic/update_process" method="post">
+                    <input type="hidden" name="id" value="${request.result[0].id}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p>
+                      <textarea name="description" placeholder="description">${description}</textarea>
+                    </p>
+                    <p>
+                      ${template.authorSelect(authors, result[0].author_id)}
+                    </p>
+                    <p>
+                      <input type="submit">
+                    </p>
+                  </form>
+                  `,
+                  `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+                );
+                response.send(html);
+              }
+          });
         }
       });
     }
@@ -87,13 +111,15 @@ router.post('/update_process', (request, response) => {
   var id = post.id;
   var title = post.title;
   var description = post.description;
-  connection.query(`UPDATE topic SET title = ?, description = ?, author_id = 1 WHERE id = ?`, [title, description, id], (error, rows, fields) => {
+  var author = post.authors;
+  connection.query(`UPDATE topic SET title = ?, description = ?, author_id = ? WHERE id = ?`, [title, description, author, id], (error, rows, fields) => {
     if (error) {
       throw error;
     } else {
       response.redirect(`/topic/${title}`);
     }
   });
+ 
 });
 
 router.post('/delete_process', (request, response) => {
@@ -110,19 +136,18 @@ router.post('/delete_process', (request, response) => {
 router.get('/:pageId', (request, response, next) => {
   var filteredId = path.parse(request.params.pageId).base;
   var title = filteredId;
-
   connection.query(`SELECT * FROM topic`, (error, rows, fields) => {
     if (error) {
       throw(error);
     } else {
-        connection.query(`SELECT * FROM topic WHERE title = "${title}"`, (error2, results) => {
+        // need to override the id in RowDataPacket by topic.id
+        connection.query(`SELECT *, topic.id FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE title = "${title}"`, (error2, results) => {
           if (error2) {
             throw error2;
           }
           else {
             request.list = rows;
             request.result = results;
-           
             var description = request.result[0].description;
             var sanitizedTitle = sanitizeHtml(title);
             var sanitizedDescription = sanitizeHtml(description, {
@@ -130,7 +155,11 @@ router.get('/:pageId', (request, response, next) => {
             });
             var list = template.list(request.list);
             var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+              `
+              <h2>${sanitizedTitle}</h2>
+              ${sanitizedDescription}
+              <p>by ${request.result[0].name}</p>
+              `,
               ` <a href="/topic/create">create</a>
                 <a href="/topic/update/${sanitizedTitle}">update</a>
                 <form action="/topic/delete_process" method="post">
