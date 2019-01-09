3841b8cfa2124974abac6ed2a80275af979288c3
Hooni <hooni@Hoonis-MacBook-Pro.local>
2019-01-09 17:54:24 -0500

    completed login/logout features with Passport

--- Nodejs/routes/auth.js
+++ Nodejs/routes/auth.js
@@ -4,18 +4,11 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 var connection = require('../lib/db');
-var auth = require('../lib/auth');
-
-var authData = {
-  id: 'hooni88',
-  password: 'test',
-  nickname: 'hooni'
-};
 
 router.get('/login', (request, response) => {
-  connection.query(`SELECT * FROM topic`, (error, topics, fields) => {
-    if (error) {
-      throw error;
+  connection.query(`SELECT * FROM topic`, (error1, topics, fields) => {
+    if (error1) {
+      throw error1;
     } else {
       connection.query(`SELECT * FROM author`, (error2, authors) => {
         if (error2) {
@@ -24,7 +17,9 @@ router.get('/login', (request, response) => {
           request.list = topics;
           var title = 'login';       
           var list = template.list(request.list);
+          var login_error = request.flash().error;
           var html = template.HTML(title, list, `
+            <p>${login_error ? login_error : ''}</p>
             <form action="/auth/login_process" method="post">
               <p><input type="text" name="id" placeholder="id"></p>
               <p>
@@ -43,24 +38,8 @@ router.get('/login', (request, response) => {
   });
 });
 
-router.post('/login_process', (request, response) => {
-  var id = request.body.id;
-  var pw = request.body.pw;
-  var nickname = authData.nickname;
-  if (id === authData.id && pw === authData.password) {
-    request.session.is_loggedin = true;
-    request.session.nickname = nickname;
-    //redirect to main page after session is saved
-    request.session.save(() => {
-      response.redirect('/');
-    });
-    
-  } else {
-    response.send('Who are you?');
-  }
-});
-
 router.get('/logout', (request, response) => {
+  request.logout();
   request.session.destroy((err) => {
     response.redirect('/');
   });
--- Nodejs/routes/index.js
+++ Nodejs/routes/index.js
@@ -5,6 +5,11 @@ var connection = require('../lib/db');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var auth = require('../lib/auth');
+var flash = require('connect-flash');
+var passport = require('passport')
+, LocalStrategy = require('passport-local').Strategy;
+
+router.use(flash());
 
 // use session middleware
 router.use(session({
@@ -14,16 +19,70 @@ router.use(session({
     store: new FileStore()
   }));
 
-router.get('/', (request, response) => {
-    /* if (request.session.num === undefined) {
-        request.session.num = 1;
-    } else {
-        request.session.num += 1;
-    }
-    if (request.session.is_loggedin) {
-        var authStatusUI = '<a href="/auth/logout">logout</a>';
-    } */
+var authData = {
+    id: 'hooni88',
+    password: 'test',
+    nickname: 'hooni'
+};
 
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    console.log('serailize ', user);
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    console.log('deserialize ', id);
+    done(null, authData);
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw'
+  },
+    function(username, password, done) {
+        console.log(username, password);
+        if (username === authData.id) {
+            if (password === authData.password) {
+                return done(null, authData)
+            } else {
+                console.log('wrong password');
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+        } else {
+            console.log('wrong id');
+            return done(null, false, { message: 'Incorrect id.' });
+        }
+    }
+));
+
+router.post('/auth/login_process', (request, response, next) => {
+    passport.authenticate('local', function(err, user, info) {
+        console.log('session', request.session);
+        if (err) { return next(err); }
+        if (!user) {
+            request.flash('error', info.message);
+            console.log('1');
+            request.session.save(() => {
+                console.log('222')
+                return response.redirect('/auth/login');
+            });
+            
+        } else {
+            request.logIn(user, function(err) {
+                console.log('3')
+                if (err) { return next(err); }
+                request.session.save(() => {
+                    return response.redirect('/');
+                });
+            });
+        }
+      })(request, response, next);
+});
+
+router.get('/', (request, response) => {
     console.log(request.session);
     connection.query(`SELECT * FROM topic`, (error, rows, fields) => {
         request.list = rows;
